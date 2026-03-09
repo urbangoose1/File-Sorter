c784ad28ecce7ea0428ccd427986d0e569a1fcf7
TheUrbanGoose <150484193+urbangoose1@users.noreply.github.com>
2026-03-09 21:12:36 +0000

    Added sorting by file type

--- FileSort.py
+++ FileSort.py
@@ -2,19 +2,44 @@ import os
 import datetime
 import shutil
 
-def MoveByDate():
-    print(r"An example: C:\Users\Name\OneDrive\Pictures\Screenshots")
+def SortByDate():
+    print(r"An example: C:\Users\John\OneDrive\Pictures\Screenshots")
     directory = input("Enter directory of files to be sorted: ").replace("\\","\\\\")
     folder = os.listdir(directory)
     for counter in range(len(folder)):
-        if os.path.isfile(directory+"\\"+folder[counter]):
-            time = os.path.getctime(directory+"\\"+folder[counter])
+        file = directory+"\\"+folder[counter]
+        if os.path.isfile(file):
+            time = os.path.getctime(file)
             date = str(datetime.datetime.fromtimestamp(time))
             try:
                 os.mkdir(directory+"\\"+date[:4])
             except FileExistsError:
                 pass
-            shutil.move(directory+"\\"+folder[counter],directory+"\\"+date[:4])
+            shutil.move(file,directory+"\\"+date[:4])
 
-MoveByDate()
-print("Files sorted by date.")
+def sortByType():
+    print(r"An example: C:\Users\John\OneDrive\Downloads")
+    directory = input("Enter directory of files to be sorted: ").replace("\\","\\\\")
+    folder = os.listdir(directory)
+    for counter in range(len(folder)):
+        file = directory+"\\"+folder[counter]
+        if os.path.isfile(file):
+            extension = os.path.splitext(file)[1]
+            try:
+                os.mkdir(directory+"\\"+extension)
+            except FileExistsError:
+                pass
+            shutil.move(file,directory+"\\"+extension)
+
+
+print("""Options:
+    [1]By date
+    [2]By extension""")
+mode = input("What do you want to sort by: ")
+print("")
+if mode == "1":
+    SortByDate()
+    print("Files sorted by date.")
+elif mode == "2":
+    sortByType()
+    print("Files sorted by extension.")
